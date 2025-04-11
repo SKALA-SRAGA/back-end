952d794f42acb09225f8d9653727b73b66552b85
Kim Se Eun <rlatpdms1014@gmail.com>
2025-04-12 03:38:39 +0900

    feat: DB 연결

---
+++ fastapi-google-stt/app/db/entity/script.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+class Script(Base):
+    __tablename__ = "script"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    file_path = Column(String(255), nullable=False)
---
+++ fastapi-google-stt/app/db/entity/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(10), unique=True, nullable=False)
+
+    script = relationship(
+        "Script",
+        cascade="all, delete-orphan",
+        lazy="selectin"  # ✅ 비동기 처리를 위해 lazy="selectin" 사용
+    )
+
