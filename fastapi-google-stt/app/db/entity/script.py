8b0ca40562e970254c296e6d17116033aba16c4f
Kim Se Eun <rlatpdms1014@gmail.com>
2025-04-13 23:45:19 +0900

    refactor: Script 테이블 id를 string으로 변경

--- fastapi-google-stt/app/db/entity/script.py
+++ fastapi-google-stt/app/db/entity/script.py
@@ -6,6 +6,6 @@ from app.db.database import Base
 class Script(Base):
     __tablename__ = "script"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String(30), primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
     file_path = Column(String(255), nullable=False)
