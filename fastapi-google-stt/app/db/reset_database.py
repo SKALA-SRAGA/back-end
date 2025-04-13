53e5f65dab3b1b31e2d4657b3933709c203cb5ba
Kim Se Eun <rlatpdms1014@gmail.com>
2025-04-13 23:44:47 +0900

    refactor: DB 연결 환경변수로 설정

--- fastapi-google-stt/app/db/reset_database.py
+++ fastapi-google-stt/app/db/reset_database.py
@@ -1,10 +1,21 @@
 from sqlalchemy.ext.asyncio import create_async_engine
+from dotenv import load_dotenv
+import os
 
 from app.db.database import Base
 
 import app.db.entity
 
-ASYNC_DB_URL = "mysql+aiomysql://root:SqlDba-1@localhost:53303/sraga?charset=utf8mb4"
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+ASYNC_DB_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 
